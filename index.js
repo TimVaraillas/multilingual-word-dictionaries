651dfe817fc25d70444185384fcfd1be9aa08dc7
Timothée Varaillas <timothee.varaillas@gmail.com>
2021-02-09 22:51:58 +0100

    Ajout des dictionnaires de mots

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-export default {};
+const de = require('./dictionaries/de.json');
+const dk = require('./dictionaries/dk.json');
+const en = require('./dictionaries/en.json');
+const es = require('./dictionaries/es.json');
+const fr = require('./dictionaries/fr.json');
+const it = require('./dictionaries/it.json');
+const nl = require('./dictionaries/nl.json');
+const no = require('./dictionaries/no.json');
+
+module.exports = {
+  de,
+  dk,
+  en,
+  es,
+  fr,
+  it,
+  nl,
+  no
+};
